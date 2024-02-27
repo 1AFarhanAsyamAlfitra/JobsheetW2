fe2dd4a87f23f87eaec2a13d7138c27a641aa014
Farhan Asyam Alfitra <asyamfarhan@gmail.com>
2024-02-27 09:07:23 +0700

    m

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,14 +9,4 @@ class WelcomeController extends Controller
     public function hello() {
         return 'Hello World';
        }
-       
-    public function index() {
-        return 'Selamat Datang';
-       }
-    public function about() {
-        return 'Farhan Asyam Alfitra 2141762138';
-       }
-    public function articles($id) {
-        return 'Halaman Artikel dengan ID ' .$id;
-       }
 }
